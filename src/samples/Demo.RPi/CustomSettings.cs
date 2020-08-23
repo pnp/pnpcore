2ab5fef13d357dd2191af3f7efb84bd3a408adba
Paul Bullock <paul.bullock@capacreative.co.uk>
2020-08-23 22:10:12 +0100

    Added sample for Raspberry Pi

---
+++ src/samples/Demo.RPi/CustomSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.RPi
+{
+    public class CustomSettings
+    {
+        public string DemoSiteUrl { get; set; }
+        public string UserPrincipalName { get; set; }
+        public string Password { get; set; }
+    }
+}
