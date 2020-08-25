c0288b0b21b0fabe5fe305a42faf540f35e05992
Paul Bullock <paul.bullock@capacreative.co.uk>
2020-08-25 09:30:53 +0100

    Added core tests class with operating system

---
+++ src/sdk/PnP.Core.Test/Base/CoreTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PnP.Core.Model.SharePoint;
+using PnP.Core.Test.Utilities;
+using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PnP.Core.Test.Base
+{
+    [TestClass]
+    public class CoreTests
+    {
+        [ClassInitialize]
+        public static void TestFixtureSetup(TestContext context)
+        {
+            
+        }
+        
+        [TestMethod]
+        public void OperatingSystemTests()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Assert.IsTrue(OperatingSystem.IsWindows());
+                Assert.IsFalse(OperatingSystem.IsMacOS());
+                Assert.IsFalse(OperatingSystem.IsLinux());
+            }
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                Assert.IsFalse(OperatingSystem.IsWindows());
+                Assert.IsFalse(OperatingSystem.IsMacOS());
+                Assert.IsTrue(OperatingSystem.IsLinux());
+            }
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                Assert.IsFalse(OperatingSystem.IsWindows());
+                Assert.IsTrue(OperatingSystem.IsMacOS());
+                Assert.IsFalse(OperatingSystem.IsLinux());
+            }
+
+        }
+
+    }
+}
