c52b4df67c8238c5a10e6ca5917716065340fc1c
Bert Jansen <bjansen@microsoft.com>
2020-08-27 10:46:08 +0200

    Commented out live test

--- src/sdk/PnP.Core.Test/SharePoint/TaxonomyTests.cs
+++ src/sdk/PnP.Core.Test/SharePoint/TaxonomyTests.cs
@@ -591,7 +591,7 @@ namespace PnP.Core.Test.SharePoint
         [TestMethod]
         public async Task AddUpdateDeleteTermProperties()
         {
-            TestCommon.Instance.Mocking = false;
+            //TestCommon.Instance.Mocking = false;
             using (var context = await TestCommon.Instance.GetContextAsync(TestCommon.TestSite))
             {
                 string newGroupName = GetGroupName(context);
