c6e11f65b4e0df1ace6a853ef505fd9f4fb02c70
Paolo Pialorsi <paolo@pialorsi.com>
2020-09-22 15:20:54 +0200

    Implemented .NET Core and DI infrastructure for PnP.Core.Auth

---
+++ src/sdk/PnP.Core.Auth/Services/IAuthenticationProviderOptions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PnP.Core.Auth.Services
+{
+    /// <summary>
+    /// Basic interface for all the Authentication Provider configurations
+    /// </summary>
+    public interface IAuthenticationProviderOptions
+    {
+        /// <summary>
+        /// The ClientId of the application to use for authentication
+        /// </summary>
+        public string ClientId { get; set; }
+
+        /// <summary>
+        /// The Tenand ID for the application, can be "organizations" for multi-tenant applications
+        /// </summary>
+        public string TenantId { get; set; }
+
+        /// <summary>
+        /// Defines the type of the IAuthenticationProvider to create
+        /// </summary>
+        public Type AuthenticationProviderType { get; }
+    }
+}
