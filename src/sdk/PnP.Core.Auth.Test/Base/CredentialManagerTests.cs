a7f0ff76246119ad26733dab2b20741bb03b3042
Paolo Pialorsi <paolo@pialorsi.com>
2020-09-22 15:25:44 +0200

    Moved CredentialManagerTests from PnP.Core.Test to PnP.Core.Auth.Test

---
+++ src/sdk/PnP.Core.Auth.Test/Base/CredentialManagerTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace PnP.Core.Auth.Test.Base
+{
+    /// <summary>
+    /// Tests that focus on validating the Credential Manager utility class
+    /// </summary>
+    [TestClass]
+    public class CredentialManagerTests
+    {
+        [ClassInitialize]
+        public static void TestFixtureSetup(TestContext context)
+        {
+            // NOOP so far
+        }
+
+        [TestMethod]
+        public void CredentialTest()
+        {
+            var userName = "userName";
+            var passWord = "password";
+            var securePassWord = "securePassword";
+            var appName = "PnP.Core.Credential.Test.SecurePw";
+
+            var result = CredentialManager.AddCredential(appName, userName, securePassWord.ToSecureString(), false);
+            Assert.IsTrue(result);
+
+            var result2 = CredentialManager.AddCredential(appName, userName, passWord, true);
+            Assert.IsTrue(result2);
+        }
+    }
+}
