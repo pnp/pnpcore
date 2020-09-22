8727a683f872346b4696a950d6221e1f101148ed
Paolo Pialorsi <paolo@pialorsi.com>
2020-09-22 15:21:48 +0200

    Implemented UsernamePasswordAuthenticationProvider

---
+++ src/sdk/PnP.Core.Auth/Delegated/UsernamePasswordAuthenticationProvider.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Client;
+using PnP.Core.Auth.Services.Builder.Configuration;
+using System;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security;
+using System.Threading.Tasks;
+
+namespace PnP.Core.Auth
+{
+    public sealed class UsernamePasswordAuthenticationProvider : OAuthAuthenticationProvider
+    {
+        /// <summary>
+        /// The username for authenticating
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// The password for authenticating
+        /// </summary>
+        public SecureString Password { get; set; }
+        
+        // Instance private member, to keep the token cache at service instance level
+        private IPublicClientApplication publicClientApplication;
+
+        /// <summary>
+        /// Public constructor leveraging DI to initialize the ILogger interfafce
+        /// </summary>
+        /// <param name="logger">The instance of the logger service provided by DI</param>
+        /// <param name="options">Configuration options for the current Authentication Provider</param>
+        public UsernamePasswordAuthenticationProvider(ILogger<OAuthAuthenticationProvider> logger)
+            : base(logger)
+        {
+        }
+
+        /// <summary>
+        /// Initializes the Authentication Provider
+        /// </summary>
+        /// <param name="options">The options to use</param>
+        public override void Init(PnPCoreAuthenticationCredentialConfigurationOptions options)
+        {
+            // We need the UsernamePassword options
+            if (options.UsernamePassword == null)
+            {
+                throw new ConfigurationErrorsException(
+                    PnPCoreAuthResources.UsernamePasswordAuthenticationProvider_InvalidConfiguration);
+            }
+
+            // We need the Username
+            if (string.IsNullOrEmpty(options.UsernamePassword.Username))
+            {
+                throw new ConfigurationErrorsException(PnPCoreAuthResources.UsernamePasswordAuthenticationProvider_InvalidUsername);
+            }
+
+            // We need the Password
+            if (string.IsNullOrEmpty(options.UsernamePassword.Password))
+            {
+                throw new ConfigurationErrorsException(PnPCoreAuthResources.UsernamePasswordAuthenticationProvider_InvalidPassword);
+            }
+
+            ClientId = !string.IsNullOrEmpty(options.ClientId) ? options.ClientId : AuthGlobals.DefaultClientId;
+            TenantId = !string.IsNullOrEmpty(options.TenantId) ? options.TenantId : AuthGlobals.OrganizationsTenantId;
+            Username = options.UsernamePassword.Username;
+            Password = options.UsernamePassword.Password.ToSecureString();
+
+            // Define the authority for the current security context
+            var authority = new Uri(String.Format(System.Globalization.CultureInfo.InvariantCulture,
+                AuthGlobals.AuthorityFormat,
+                TenantId));
+
+            // Build the MSAL client
+            publicClientApplication = PublicClientApplicationBuilder
+                .Create(ClientId)
+                .WithAuthority(authority)
+                .Build();
+        }
+
+        /// <summary>
+        /// Authenticates the specified request message.
+        /// </summary>
+        /// <param name="resource">Request uri</param>
+        /// <param name="request">The <see cref="HttpRequestMessage"/> to authenticate.</param>
+        /// <returns>The task to await.</returns>
+        public override async Task AuthenticateRequestAsync(Uri resource, HttpRequestMessage request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+
+            if (resource == null)
+            {
+                throw new ArgumentNullException(nameof(resource));
+            }
+
+            request.Headers.Authorization = new AuthenticationHeaderValue("bearer", 
+                await GetAccessTokenAsync(resource).ConfigureAwait(false));
+        }
+
+        /// <summary>
+        /// Gets an access token for the requested resource and scope
+        /// </summary>
+        /// <param name="resource">Resource to request an access token for</param>
+        /// <param name="scopes">Scopes to request</param>
+        /// <returns>An access token</returns>
+        public override async Task<string> GetAccessTokenAsync(Uri resource, string[] scopes)
+        {
+            if (resource == null)
+            {
+                throw new ArgumentNullException(nameof(resource));
+            }
+
+            if (scopes == null)
+            {
+                throw new ArgumentNullException(nameof(scopes));
+            }
+
+            AuthenticationResult tokenResult = null;
+
+            var account = await publicClientApplication.GetAccountsAsync().ConfigureAwait(false);
+            try
+            {
+                // Try to get the token from the tokens cache
+                tokenResult = publicClientApplication.AcquireTokenSilent(scopes, account.First())
+                    .ExecuteAsync().GetAwaiter().GetResult();
+            }
+            catch
+            {
+                // Try to get the token directly through AAD if it is not available in the tokens cache
+                tokenResult = publicClientApplication.AcquireTokenByUsernamePassword(scopes, Username, Password)
+                    .ExecuteAsync().GetAwaiter().GetResult();
+            }
+
+            // Return the Access Token, if we've got it
+            // In case of any exception while retrieving the access token, 
+            // MSAL will throw an exception that we simply bubble up
+            return tokenResult.AccessToken;
+        }
+
+        /// <summary>
+        /// Gets an access token for the requested resource 
+        /// </summary>
+        /// <param name="resource">Resource to request an access token for</param>
+        /// <returns>An access token</returns>
+        public override async Task<string> GetAccessTokenAsync(Uri resource)
+        {
+            if (resource == null)
+            {
+                throw new ArgumentNullException(nameof(resource));
+            }
+
+            // Use the .default scope if the scopes are not provided
+            return await GetAccessTokenAsync(resource, 
+                new string[] { $"{resource.Scheme}://{resource.Authority}/.default" }).ConfigureAwait(false);
+        }
+    }
+}
