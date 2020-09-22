d29923025bcb252a37d290812721ae666fe3e688
Paolo Pialorsi <paolo@pialorsi.com>
2020-09-22 12:01:08 +0200

    Removed AssemblyInfo.cs from PnP.Core

--- src/sdk/PnP.Core/AssemblyInfo.cs
+++
@@ -1,2 +0,0 @@
-﻿// The following attribute specifies that the internal types of PnP.Core are visible to PnP.Core.Auth assembly
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("PnP.Core.Auth")]
