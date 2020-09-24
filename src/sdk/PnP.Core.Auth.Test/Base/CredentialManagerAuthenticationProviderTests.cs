bd7d0e271c64bebc3564375b17a68d6ce01dc119
Paolo Pialorsi <paolo@pialorsi.com>
2020-09-24 16:09:21 +0200

    Updated and split test for every single Authentication Provider

---
+++ src/sdk/PnP.Core.Auth.Test/Base/CredentialManagerAuthenticationProviderTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PnP.Core.Auth.Test.Utilities;
+using PnP.Core.Model;
+using PnP.Core.Services;
+using System.Threading.Tasks;
+
+namespace PnP.Core.Auth.Test.Base
+{
+    /// <summary>
+    /// Tests that focus on validating the CredentialManagerAuthenticationProvider
+    /// </summary>
+    [TestClass]
+    public class CredentialManagerAuthenticationProviderTests
+    {
+        [ClassInitialize]
+        public static void TestFixtureSetup(TestContext context)
+        {
+            // NOOP so far
+        }
+
+        [TestMethod]
+        public async Task TestCredentialManagerWithGraph()
+        {
+            if (TestCommon.RunningInGitHubWorkflow()) Assert.Inconclusive("Skipping live test because we're running inside a GitHub action");
+
+            using (var context = await TestCommon.Instance.GetContextAsync(TestCommon.TestSiteCredentialManager))
+            {
+                await TestCommon.CheckAccessToTargetResource(context);
+            }
+        }
+
+        [TestMethod]
+        public async Task TestCredentialManagerWithSPO()
+        {
+            if (TestCommon.RunningInGitHubWorkflow()) Assert.Inconclusive("Skipping live test because we're running inside a GitHub action");
+
+            using (var context = await TestCommon.Instance.GetContextAsync(TestCommon.TestSiteCredentialManager))
+            {
+                await TestCommon.CheckAccessToTargetResource(context, false);
+            }
+        }
+    }
+}
