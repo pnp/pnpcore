ce18b090d6c3cb2953c7dba5aadbab46d043a94e
Paolo Pialorsi <paolo@pialorsi.com>
2020-09-24 17:52:01 +0200

    Implemented tests for CredentialManagerAuthenticationProvider

--- src/sdk/PnP.Core.Auth.Test/Base/InteractiveAuthenticationProviderTests.cs
+++ src/sdk/PnP.Core.Auth.Test/Base/InteractiveAuthenticationProviderTests.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PnP.Core.Auth.Test.Utilities;
 using PnP.Core.Model;
 using PnP.Core.Services;
+using System;
+using System.Configuration;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace PnP.Core.Auth.Test.Base
@@ -12,6 +16,9 @@ namespace PnP.Core.Auth.Test.Base
     [TestClass]
     public class InteractiveAuthenticationProviderTests
     {
+        private static Uri graphResource = new Uri("https://graph.microsoft.com");
+        private static string graphMeRequest = "https://graph.microsoft.com/v1.0/me";
+        
         [ClassInitialize]
         public static void TestFixtureSetup(TestContext context)
         {
@@ -38,6 +45,136 @@ namespace PnP.Core.Auth.Test.Base
             {
                 await TestCommon.CheckAccessToTargetResource(context, false);
             }
+        }
+
+        [TestMethod]
+        public async Task TestInteractiveConstructorNoDI()
+        {
+            var provider = PrepareInteractiveAuthenticationProvider();
+
+            Assert.IsNotNull(provider);
+            Assert.IsNotNull(provider.ClientId);
+            Assert.IsNotNull(provider.TenantId);
+            Assert.IsNotNull(provider.RedirectUri);
+        }
+
+        [TestMethod]
+        public async Task TestInteractiveConstructorNoDI_NullClientId_NullTenantId()
+        {
+            var configuration = TestCommon.GetConfigurationSettings();
+            var clientId = configuration.GetValue<string>("PnPCore:Credentials:Configurations:TestSiteInteractive:ClientId");
+            var tenantId = configuration.GetValue<string>("PnPCore:Credentials:Configurations:TestSiteInteractive:TenantId");
+            var redirectUri = configuration.GetValue<Uri>("PnPCore:Credentials:Configurations:TestSiteInteractive:Interactive:RedirectUri");
+
+            var provider = new InteractiveAuthenticationProvider(
+                clientId,
+                tenantId,
+                redirectUri);
+
+            Assert.IsNotNull(provider);
+            Assert.IsNotNull(provider.ClientId);
+            Assert.IsNotNull(provider.TenantId);
+            Assert.IsNotNull(provider.RedirectUri);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ConfigurationErrorsException))]
+        public async Task TestInteractiveConstructorNoDI_NullRedirectUri()
+        {
+            var configuration = TestCommon.GetConfigurationSettings();
+            var clientId = configuration.GetValue<string>("PnPCore:Credentials:Configurations:TestSiteInteractive:ClientId");
+            var tenantId = configuration.GetValue<string>("PnPCore:Credentials:Configurations:TestSiteInteractive:TenantId");
+
+            var provider = new InteractiveAuthenticationProvider(
+                clientId,
+                tenantId,
+                null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async Task TestInteractiveAuthenticateRequestAsyncNoResource()
+        {
+            var provider = PrepareInteractiveAuthenticationProvider();
+
+            await provider.AuthenticateRequestAsync(null, null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async Task TestInteractiveAuthenticateRequestAsyncNoHttpRequest()
+        {
+            var provider = PrepareInteractiveAuthenticationProvider();
+
+            await provider.AuthenticateRequestAsync(graphResource, null);
+        }
+
+        [TestMethod]
+        public async Task TestInteractiveAuthenticateRequestAsyncCorrect()
+        {
+            if (TestCommon.RunningInGitHubWorkflow()) Assert.Inconclusive("Skipping live test because we're running inside a GitHub action");
+
+            var provider = PrepareInteractiveAuthenticationProvider();
+
+            var request = new HttpRequestMessage(HttpMethod.Get, graphMeRequest);
+            await provider.AuthenticateRequestAsync(graphResource, request);
+
+            Assert.IsNotNull(request.Headers.Authorization);
+            Assert.AreEqual(request.Headers.Authorization.Scheme.ToLower(), "bearer");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async Task TestInteractiveGetAccessTokenAsyncNullResource()
+        {
+            var provider = PrepareInteractiveAuthenticationProvider();
+
+            await provider.GetAccessTokenAsync(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async Task TestInteractiveGetAccessTokenAsyncFullNullResource()
+        {
+            var provider = PrepareInteractiveAuthenticationProvider();
+
+            await provider.GetAccessTokenAsync(null, new string[] { });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async Task TestInteractiveGetAccessTokenAsyncFullNullScopes()
+        {
+            var provider = PrepareInteractiveAuthenticationProvider();
+
+            var accessToken = await provider.GetAccessTokenAsync(graphResource, null);
+        }
+
+        [TestMethod]
+        public async Task TestInteractiveGetAccessTokenAsyncCorrect()
+        {
+            if (TestCommon.RunningInGitHubWorkflow()) Assert.Inconclusive("Skipping live test because we're running inside a GitHub action");
+
+            var provider = PrepareInteractiveAuthenticationProvider();
+
+            var accessToken = await provider.GetAccessTokenAsync(graphResource);
+
+            Assert.IsNotNull(accessToken);
+            Assert.IsTrue(accessToken.Length > 0);
+        }
+
+        private static InteractiveAuthenticationProvider PrepareInteractiveAuthenticationProvider()
+        {
+            var configuration = TestCommon.GetConfigurationSettings();
+            var clientId = configuration.GetValue<string>("PnPCore:Credentials:Configurations:TestSiteInteractive:ClientId");
+            var tenantId = configuration.GetValue<string>("PnPCore:Credentials:Configurations:TestSiteInteractive:TenantId");
+            var redirectUri = configuration.GetValue<Uri>("PnPCore:Credentials:Configurations:TestSiteInteractive:Interactive:RedirectUri");
+
+            var provider = new InteractiveAuthenticationProvider(
+                clientId,
+                tenantId,
+                redirectUri);
+            return provider;
         }
     }
 }
