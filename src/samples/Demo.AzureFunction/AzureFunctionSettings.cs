bc8b3bbf7faa05c6d8df12505ed0988ae656ca6e
Yannick Plenevaux <yannick.plenevaux@pvx-solutions.com>
2020-09-27 23:05:10 +0200

    Azure Function v3 sample

---
+++ src/samples/Demo.AzureFunction/AzureFunctionSettings.cs
@@ -0,0 +1,18 @@
+﻿using PnP.Core.Auth.Services.Builder.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Demo.AzureFunction
+{
+    class AzureFunctionSettings
+    {
+        public string SiteUrl { get; set; }
+        public string TenantId { get; set; }
+        public string ClientId { get; set; }
+        public StoreName CertificateStoreName { get; set; }
+        public StoreLocation CertificateStoreLocation { get; set; }
+        public string CertificateThumbprint { get; set; }
+    }
+}
---
+++ src/samples/Demo.AzureFunction/Configure.ps1
@@ -0,0 +1,30 @@
+﻿param (
+	[Parameter(Mandatory=$true)]
+	[string]
+	$SiteUrl,
+	[Parameter(Mandatory=$true)]
+	[string]
+	$Tenant,
+	[Parameter(Mandatory=$true)]
+	[string]
+	$CertificatePassword,
+	[string]$AzureADAppName = "PnP.Core.SDK.AzureFunctionSample",
+	[string]$CertificateOutDir = ".\Certificates"
+)
+
+Connect-PnPOnline $SiteUrl
+
+if (-not ( Test-Path -Path $CertificateOutDir -PathType Container )) {
+	md $CertificateOutDir
+}
+
+$app = Initialize-PnPPowerShellAuthentication -ApplicationName $AzureADAppName -Tenant $Tenant -OutPath $CertificateOutDir -CertificatePassword (ConvertTo-SecureString -String $CertificatePassword -AsPlainText -Force) -Scopes "MSGraph.Group.ReadWrite.All","MSGraph.User.ReadWrite.All","SPO.Sites.FullControl.All","SPO.TermStore.ReadWrite.All","SPO.User.ReadWrite.All" -Store CurrentUser
+
+$localSettings = Get-Content local.settings.sample.json | ConvertFrom-JSON
+$localSettings.Values.SiteUrl = $SiteUrl
+$localSettings.Values.TenantId = Get-PnPTenantId
+$localSettings.Values.ClientId = $app.AzureAppId
+$localSettings.Values.CertificateThumbPrint = $app.'Certificate Thumbprint' 
+$localSettings.Values.WEBSITE_LOAD_CERTIFICATES = $app.'Certificate Thumbprint'
+
+($localSettings | ConvertTo-JSON) > local.settings.json
