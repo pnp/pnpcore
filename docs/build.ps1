9557eeae3df1736df66671056e67038ee1271001
Paul Bullock <paul.bullock@capacreative.co.uk>
2020-09-30 19:06:25 +0100

    Working build alternative

--- docs/build.ps1
+++ docs/build.ps1
@@ -3,6 +3,23 @@
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version 2.0
 
+New-Item -Path ./dev/docs -Name "demos" -ItemType "directory"
+copy-item -Force ./dev/src/samples/* -Destination ./dev/docs/demos
+Get-item ./src/samples/* | Foreach-Object {
+  if($_.PSIsContainer){
+      $_.BaseName
+      Copy-Item "./src/samples/$($_.Name)/*.md" -Destination "./docs/demos/$($_.Name)" -Force
+      Copy-Item "./src/samples/$($_.Name)/*.png" -Destination "./docs/demos/$($_.Name)" -Force
+
+      if(Test-Path "./src/samples/$($_.Name)/doc-images"){
+        Copy-Item "./src/samples/$($_.Name)/doc-images/*.png" -Destination "./docs/demos/$($_.Name)" -Force
+      }
+      if(Test-Path "./src/samples/$($_.Name)/assets"){
+        Copy-Item "./src/samples/$($_.Name)/assets/*.png" -Destination "./docs/demos/$($_.Name)" -Force
+      }
+  }
+}
+
 docfx metadata ./dev/docs/docfx.json --warningsAsErrors $args
 docfx build ./dev/docs/docfx.json --warningsAsErrors $args
 
@@ -11,3 +28,10 @@ Remove-Item ./gh-pages/api/* -Recurse -Force
 Remove-Item ./gh-pages/articles/* -Recurse -Force
 Remove-Item ./gh-pages/images/* -Recurse -Force
 copy-item -Force -Recurse ./dev/docs/_site/* -Destination ./gh-pages
+
+
+Get-item ./src/samples/* | % { 
+    if($_.PSIsContainer){
+        Copy-Item *.md -Destination $_.FullName
+    }
+}
