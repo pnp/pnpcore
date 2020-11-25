b7dc267a25bc6f729181f9a5a8ab60b542c2a5ea
Bert Jansen <bjansen@microsoft.com>
2020-11-25 06:14:34 +0100

    Port to .NET 5.0

--- src/samples/Demo.Blazor/Program.cs
+++ src/samples/Demo.Blazor/Program.cs
@@ -3,31 +3,33 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PnP.Core.Services;
-using PnP.Core.Services.Builder.Configuration;
-using System.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Demo.Blazor
 {
     public class Program
     {
-        //public const string AuthProviderName = "AccessTokenAuth";
-
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            var config = new ConfigurationBuilder().Build();
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddMsalAuthentication(options =>
             {
                 builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+                // Provide a default scope, need to get Msal.js to work
+                options.ProviderOptions.DefaultAccessTokenScopes = new List<string>() { "https://graph.microsoft.com/.default" };
             });
 
             // Add the PnP Core SDK library
             builder.Services.AddPnPCore();
-            
+
+            var config = new ConfigurationBuilder().Build();
             builder.Services
                 // Add our custom IAuthenticationProvider implementation
                 .AddScoped<IAuthenticationProvider, MsalWrappedTokenProvider>()
