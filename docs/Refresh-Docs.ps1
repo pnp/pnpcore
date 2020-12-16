7faf11f55c400993a7d962639b4e900af03e17bf
Bert Jansen <bjansen@microsoft.com>
2020-12-16 13:20:31 +0100

    fix doc build script after restructure

--- docs/Refresh-Docs.ps1
+++ docs/Refresh-Docs.ps1
@@ -8,6 +8,7 @@ docfx.exe build docfx.json
 
 # Copy the created site to the pnpcoredocs folder (= clone of the gh-pages branch)
 Remove-Item $gitDrive\github\pnpcoredocs\api\* -Recurse -Force
-Remove-Item $gitDrive\github\pnpcoredocs\articles\* -Recurse -Force
+Remove-Item $gitDrive\github\pnpcoredocs\using-the-sdk\* -Recurse -Force
+Remove-Item $gitDrive\github\pnpcoredocs\contributing\* -Recurse -Force
 Remove-Item $gitDrive\github\pnpcoredocs\images\* -Recurse -Force
 copy-item -Force -Recurse .\_site\* -Destination $gitDrive\github\pnpcoredocs
