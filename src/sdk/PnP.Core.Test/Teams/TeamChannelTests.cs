8402122d891092689264e212cb008cfc8873f4b3
Paul Bullock <paul.bullock@capacreative.co.uk>
2020-12-18 14:13:00 +0000

    More Team Channel Tests

--- src/sdk/PnP.Core.Test/Teams/TeamChannelTests.cs
+++ src/sdk/PnP.Core.Test/Teams/TeamChannelTests.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PnP.Core.Model.Teams;
 using PnP.Core.Test.Utilities;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,44 +16,7 @@ namespace PnP.Core.Test.Teams
             // Configure mocking default for all tests in this class, unless override by a specific test
             //TestCommon.Instance.Mocking = false;
         }
-
-        //[TestMethod]
-        //public async Task GetFunSettings()
-        //{
-        //    //TestCommon.Instance.Mocking = false;
-        //    using (var context = await TestCommon.Instance.GetContextAsync(TestCommon.TestSite))
-        //    {
-        //        var team = await context.Team.GetAsync(x => x.FunSettings);
-        //        Assert.IsNotNull(team.FunSettings);
-        //    }
-        //}
-
-        //[TestMethod]
-        //public async Task UpdateMessagingSettings()
-        //{
-        //    //TestCommon.Instance.Mocking = false;
-        //    using (var context = await TestCommon.Instance.GetContextAsync(TestCommon.TestSite))
-        //    {
-        //        var team = await context.Team.GetAsync(x => x.MessagingSettings);
-
-        //        team.MessagingSettings.AllowChannelMentions = true;
-        //        team.MessagingSettings.AllowOwnerDeleteMessages = true;
-        //        team.MessagingSettings.AllowTeamMentions = true;
-        //        team.MessagingSettings.AllowUserDeleteMessages = true;
-        //        team.MessagingSettings.AllowUserEditMessages = true;
-
-        //        await team.UpdateAsync();
-
-        //        team = await context.Team.GetAsync(x => x.MessagingSettings);
-
-        //        Assert.IsTrue(team.MessagingSettings.AllowChannelMentions);
-        //        Assert.IsTrue(team.MessagingSettings.AllowOwnerDeleteMessages);
-        //        Assert.IsTrue(team.MessagingSettings.AllowTeamMentions);
-        //        Assert.IsTrue(team.MessagingSettings.AllowUserDeleteMessages);
-        //        Assert.IsTrue(team.MessagingSettings.AllowUserEditMessages);
-        //    }
-        //}
-
+                
         [TestMethod]
         public async Task GetChannelsAsyncTest()
         {
@@ -91,6 +55,22 @@ namespace PnP.Core.Test.Teams
                 Assert.AreEqual(channel.MembershipType, TeamChannelMembershipType.Standard);
                 Assert.IsNotNull(channel.WebUrl);
 
+            }
+        }
+
+        [TestMethod]
+        public void GetGeneralChannelTest()
+        {
+            //TestCommon.Instance.Mocking = false;
+            using (var context = TestCommon.Instance.GetContext(TestCommon.TestSite))
+            {
+                var team = context.Team.Get(o => o.Channels);
+                Assert.IsTrue(team.Channels.Length > 0);
+
+                var channel = team.Channels.GetByDisplayName("General");
+
+                Assert.AreEqual(channel.MembershipType, TeamChannelMembershipType.Standard);
+                Assert.IsNotNull(channel.WebUrl);
             }
         }
 
