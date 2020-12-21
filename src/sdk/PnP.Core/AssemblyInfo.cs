076254837be52ee3c9a7ec983fd43b634cdd4ba2
Bert Jansen <bjansen@microsoft.com>
2020-12-21 15:46:45 +0100

    Internals are visible for PnP PowerShell to support deep integration

--- src/sdk/PnP.Core/AssemblyInfo.cs
+++ src/sdk/PnP.Core/AssemblyInfo.cs
@@ -3,12 +3,14 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("PnP.Core.Auth")]
 [assembly: InternalsVisibleTo("PnP.Framework")]
+[assembly: InternalsVisibleTo("PnP.PowerShell")]
 
 #if DEBUG
 [assembly: InternalsVisibleTo("PnP.Core.Test")]
 [assembly: InternalsVisibleTo("PnP.Core.Auth.Test")]
 [assembly: InternalsVisibleTo("PnP.Framework.Test")]
 [assembly: InternalsVisibleTo("PnP.Framework.Modernization.Test")]
+[assembly: InternalsVisibleTo("PnP.PowerShell.Test")]
 #endif
 
 [assembly: NeutralResourcesLanguage("en")]
