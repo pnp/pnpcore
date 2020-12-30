bd044b45722d46f77aca94587fcdf0f0452c049f
Paul Bullock <paul.bullock@capacreative.co.uk>
2020-12-30 09:37:07 +0000

    Added exception tests to channels and minor bug fix

--- src/sdk/PnP.Core.Test/Teams/TeamChannelTests.cs
+++ src/sdk/PnP.Core.Test/Teams/TeamChannelTests.cs
@@ -91,5 +91,35 @@ namespace PnP.Core.Test.Teams
 
             }
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetChannelNameExceptionTest()
+        {
+            //TestCommon.Instance.Mocking = false;
+            using (var context = TestCommon.Instance.GetContext(TestCommon.TestSite))
+            {
+                var team = context.Team.Get(o => o.Channels);
+                Assert.IsTrue(team.Channels.Length > 0);
+
+                var channel = team.Channels.GetByDisplayName(null);
+
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async Task GetChannelNameExceptionAsyncTest()
+        {
+            //TestCommon.Instance.Mocking = false;
+            using (var context = await TestCommon.Instance.GetContextAsync(TestCommon.TestSite))
+            {
+                var team = await context.Team.GetAsync(o => o.Channels);
+                Assert.IsTrue(team.Channels.Length > 0);
+
+                var channel = await team.Channels.GetByDisplayNameAsync("");
+
+            }
+        }
     }
 }
