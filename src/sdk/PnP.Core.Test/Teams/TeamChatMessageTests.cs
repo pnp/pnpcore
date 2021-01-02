e36453dcddbb5d605b869583bea8652c5e60a7d2
Paul Bullock <paul.bullock@capacreative.co.uk>
2021-01-02 22:22:30 +0000

    Added get/add chat message to channel tests

---
+++ src/sdk/PnP.Core.Test/Teams/TeamChatMessageTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PnP.Core.Model.Teams;
+using PnP.Core.Test.Utilities;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PnP.Core.Test.Teams
+{
+    [TestClass]
+    public class TeamChatMessageTests
+    {
+        [ClassInitialize]
+        public static void TestFixtureSetup(TestContext context)
+        {
+            // Configure mocking default for all tests in this class, unless override by a specific test
+            //TestCommon.Instance.Mocking = false;
+        }
+                
+        [TestMethod]
+        public async Task GetChatMessageAsyncTest()
+        {
+            //TestCommon.Instance.Mocking = false;
+            using (var context = await TestCommon.Instance.GetContextAsync(TestCommon.TestSite))
+            {
+                var team = await context.Team.GetAsync(o => o.Channels);
+                Assert.IsTrue(team.Channels.Length > 0);
+
+                var channel = team.Channels.FirstOrDefault(i => i.DisplayName == "General");
+                Assert.IsNotNull(channel);
+
+                channel = await channel.GetAsync(o => o.Messages);
+                var chatMessages = channel.Messages;
+
+                Assert.IsNotNull(chatMessages);
+
+                // assume as if there are no chat messages
+                // There appears to be no remove option yet in this feature - so add a recognisable message
+                var body = "Hello, this is a unit test (GetChatMessageAsyncTest) posting a message - PnP Rocks!";
+                if(!chatMessages.Any(o=> o.Body.Content == body))
+                {
+                    await chatMessages.AddAsync(body);
+                }
+
+                channel = await channel.GetAsync(o => o.Messages);
+                var updateMessages = channel.Messages;
+
+                var message = updateMessages.First(o => o.Body.Content == body);
+                Assert.IsNotNull(message.CreatedDateTime);
+                Assert.AreEqual(message.DeletedDateTime, DateTime.MinValue);
+                Assert.IsNotNull(message.Etag);
+                Assert.IsNotNull(message.Importance);
+                Assert.IsNotNull(message.LastModifiedDateTime);
+                Assert.IsNotNull(message.Locale);
+                Assert.IsNotNull(message.MessageType);
+                Assert.IsNotNull(message.WebUrl);
+
+                Assert.IsTrue(message.IsPropertyAvailable(o => o.ReplyToId));
+                Assert.IsNull(message.ReplyToId);
+                Assert.IsTrue(message.IsPropertyAvailable(o => o.Subject));
+                Assert.IsNull(message.Subject);
+                Assert.IsTrue(message.IsPropertyAvailable(o => o.Summary));
+                Assert.IsNull(message.Summary);
+                
+            }
+        }
+
+    }
+}
