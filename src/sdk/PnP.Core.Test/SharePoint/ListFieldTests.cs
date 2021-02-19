5fca8cff6154b707139df008becdb26068dfac46
Bert Jansen <bjansen@microsoft.com>
2021-02-19 09:11:46 +0100

    ListFieldTests update

--- src/sdk/PnP.Core.Test/SharePoint/ListFieldTests.cs
+++ src/sdk/PnP.Core.Test/SharePoint/ListFieldTests.cs
@@ -5,7 +5,6 @@ using PnP.Core.Test.Utilities;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
-using PnP.Core.Model;
 
 namespace PnP.Core.Test.SharePoint
 {
@@ -26,9 +25,9 @@ namespace PnP.Core.Test.SharePoint
             using (var context = await TestCommon.Instance.GetContextAsync(TestCommon.TestSite))
             {
                 var documents = context.Web.Lists.GetByTitle("Documents", l => l.Fields);
-                Assert.IsTrue(documents.Fields.Count() > 0);
+                Assert.IsTrue(documents.Fields.Length > 0);
 
-                IField field = documents.Fields.FirstOrDefault(p => p.InternalName == "Title");
+                IField field = documents.Fields.AsEnumerable().FirstOrDefault(p => p.InternalName == "Title");
                 // Test a string property
                 Assert.AreEqual("Title", field.InternalName);
                 // Test a boolean property
