62992806f0a9c06e4658a49f2515b400b64dfa6c
Paul Bullock <paul.bullock@capacreative.co.uk>
2021-02-27 20:51:54 +0000

    Updated to PnP.PowerShell + migrated

--- samples/Demo.AzureFunction/Configure.ps1
+++ samples/Demo.AzureFunction/Configure.ps1
@@ -12,18 +12,19 @@
 	[string]$CertificateOutDir = ".\Certificates"
 )
 
-Connect-PnPOnline $SiteUrl
-
 if (-not ( Test-Path -Path $CertificateOutDir -PathType Container )) {
 	md $CertificateOutDir
 }
 
-$app = Initialize-PnPPowerShellAuthentication -ApplicationName $AzureADAppName -Tenant $Tenant -OutPath $CertificateOutDir -CertificatePassword (ConvertTo-SecureString -String $CertificatePassword -AsPlainText -Force) -Scopes "MSGraph.Group.ReadWrite.All","MSGraph.User.ReadWrite.All","SPO.Sites.FullControl.All","SPO.TermStore.ReadWrite.All","SPO.User.ReadWrite.All" -Store CurrentUser
+$app = Register-PnPAzureADApp -ApplicationName $AzureADAppName -Tenant $Tenant -OutPath $CertificateOutDir `
+	-CertificatePassword (ConvertTo-SecureString -String $CertificatePassword -AsPlainText -Force) `
+	-Scopes "MSGraph.Group.ReadWrite.All","MSGraph.User.ReadWrite.All","SPO.Sites.FullControl.All","SPO.TermStore.ReadWrite.All","SPO.User.ReadWrite.All" `
+	-Store CurrentUser -DeviceLogin
 
 $localSettings = Get-Content local.settings.sample.json | ConvertFrom-JSON
 $localSettings.Values.SiteUrl = $SiteUrl
 $localSettings.Values.TenantId = Get-PnPTenantId
-$localSettings.Values.ClientId = $app.AzureAppId
+$localSettings.Values.ClientId = $app.'AzureAppId/ClientId'
 $localSettings.Values.CertificateThumbPrint = $app.'Certificate Thumbprint' 
 $localSettings.Values.WEBSITE_LOAD_CERTIFICATES = $app.'Certificate Thumbprint'
 
