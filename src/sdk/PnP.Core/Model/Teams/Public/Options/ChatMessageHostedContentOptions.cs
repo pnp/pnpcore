95cf2bb9ae6d32f4219a9a9dfe9373b7160bb0e1
Paul Bullock <paul.bullock@capacreative.co.uk>
2021-03-13 22:04:18 +0000

    Refactoered chat message and bug fix ref issue #351

---
+++ src/sdk/PnP.Core/Model/Teams/Public/Options/ChatMessageHostedContentOptions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PnP.Core.Model.Teams
+{
+    /// <summary>
+    /// Chat Message Hosted Content options
+    /// </summary>
+    public class ChatMessageHostedContentOptions
+    {
+        /// <summary>
+        /// Hosted Content Id
+        /// </summary>
+        public string Id { get; set; }
+            
+        /// <summary>
+        /// Hosted content type
+        /// </summary>
+        public string ContentType { get; set; }
+        
+        /// <summary>
+        /// Hosted content bytes
+        /// </summary>
+        public string ContentBytes { get; set; }
+    }
+}
---
+++ src/sdk/PnP.Core/Model/Teams/Public/Options/ChatMessageOptions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PnP.Core.Model.Teams
+{
+    /// <summary>
+    /// Chat Message options
+    /// </summary>
+    public class ChatMessageOptions
+    {
+        /// <summary>
+        /// Message content
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        /// Type of the message content
+        /// </summary>
+        public ChatMessageContentType ContentType { get; set; } = ChatMessageContentType.Text;
+
+        /// <summary>
+        /// Message Subject
+        /// </summary>
+        public string Subject { get; set; }
+
+        /// <summary>
+        /// Attachment options
+        /// </summary>
+        public List<ChatMessageAttachmentOptions> Attachments { get; private set; } = new List<ChatMessageAttachmentOptions>();
+
+        /// <summary>
+        /// Message hosted content options
+        /// </summary>
+        public List<ChatMessageHostedContentOptions> HostedContents { get; private set; } = new List<ChatMessageHostedContentOptions>();
+    }
+}
