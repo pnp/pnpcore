7f6c5e09846eb33e382b03bbff451985f0816311
Cristian Civera <cristian@cristiancivera.com>
2021-05-18 14:45:03 +0200

    Added test

---
+++ src/sdk/PnP.Core.Transformation.Test/TransformationTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PnP.Core.Transformation.Services.Builder.Configuration;
+using PnP.Core.Transformation.Services.Core;
+using PnP.Core.Transformation.Services.MappingProviders;
+using PnP.Core.Services;
+
+namespace PnP.Core.Transformation.Test
+{
+    [TestClass]
+    public class TransformationTests
+    {
+
+        [TestMethod]
+        public async Task TransformAsync()
+        {
+            var services = new ServiceCollection();
+            services.AddLogging();
+            services.AddPnPTransformation();
+
+            var provider = services.BuildServiceProvider();
+
+            var pageTransformator = provider.GetRequiredService<IPageTransformator>();
+            await pageTransformator.TransformAsync(new PageTransformationTask());
+        }
+        
+    }
+}
--- src/sdk/PnP.Core.Transformation/Services/Core/DefaultPageTransformator.cs
+++ src/sdk/PnP.Core.Transformation/Services/Core/DefaultPageTransformator.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
 using PnP.Core.Transformation.Services.MappingProviders;
 
 namespace PnP.Core.Transformation.Services.Core
@@ -14,16 +15,19 @@ namespace PnP.Core.Transformation.Services.Core
     {
         private readonly ILogger<DefaultPageTransformator> logger;
         private readonly IMappingProvider mappingProvider;
+        private readonly IOptions<PageTransformationOptions> defaultPageTransformationOptions;
 
         /// <summary>
         /// Constructor with DI support
         /// </summary>
         /// <param name="logger">The logger interface</param>
         /// <param name="mappingProvider">The mapping provider interface</param>
-        public DefaultPageTransformator(ILogger<DefaultPageTransformator> logger, IMappingProvider mappingProvider)
+        /// <param name="defaultPageTransformationOptions"></param>
+        public DefaultPageTransformator(ILogger<DefaultPageTransformator> logger, IMappingProvider mappingProvider, IOptions<PageTransformationOptions> defaultPageTransformationOptions)
         {
             this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
             this.mappingProvider = mappingProvider ?? throw new ArgumentNullException(nameof(mappingProvider));
+            this.defaultPageTransformationOptions = defaultPageTransformationOptions ?? throw new ArgumentNullException(nameof(defaultPageTransformationOptions));
         }
 
         /// <summary>
@@ -36,10 +40,14 @@ namespace PnP.Core.Transformation.Services.Core
         {
             this.logger.LogInformation("TransformAsync");
 
-            //var context = new PageTransformationContext(task, options);
+            // Create a custom options instance for this page
+            var pageTransformationOptions = new PageTransformationOptions(defaultPageTransformationOptions.Value);
+            options?.Invoke(pageTransformationOptions);
+
+            var context = new PageTransformationContext(task, pageTransformationOptions);
             var input = new MappingProviderInput
             {
-
+                Context = context
             };
             await this.mappingProvider.MapAsync(input).ConfigureAwait(false);
 
