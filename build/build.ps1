4000cb22954e4ed1cd5fd2b839d40040cdcd9548
Bert Jansen <bjansen@microsoft.com>
2021-09-16 08:51:33 +0200

    Use Release version in nightly builds

--- build/build.ps1
+++ build/build.ps1
@@ -18,14 +18,14 @@ Write-Host "Building PnP.Core.Auth versions $version"
 dotnet build ./src/sdk/PnP.Core.Auth/PnP.Core.Auth.csproj --configuration Release --no-incremental /p:Version=$version
 
 Write-Host "Packinging PnP.Core versions $version"
-dotnet pack ./src/sdk/PnP.Core/PnP.Core.csproj --no-build /p:PackageVersion=$version
+dotnet pack ./src/sdk/PnP.Core/PnP.Core.csproj --configuration Release --no-build /p:PackageVersion=$version
 
 Write-Host "Packinging PnP.Core.Auth versions $version"
-dotnet pack ./src/sdk/PnP.Core.Auth/PnP.Core.Auth.csproj --no-build /p:PackageVersion=$version
+dotnet pack ./src/sdk/PnP.Core.Auth/PnP.Core.Auth.csproj --configuration Release --no-build /p:PackageVersion=$version
 
 Write-Host "Publishing to nuget"
-$nupkg = $("./src/sdk/PnP.Core/bin/Debug/PnP.Core.$version.nupkg")
-$authNupkg = $("./src/sdk/PnP.Core.Auth/bin/Debug/PnP.Core.Auth.$version.nupkg")
+$nupkg = $("./src/sdk/PnP.Core/bin/Release/PnP.Core.$version.nupkg")
+$authNupkg = $("./src/sdk/PnP.Core.Auth/bin/Release/PnP.Core.Auth.$version.nupkg")
 $apiKey = $("$env:NUGET_API_KEY")
 
 #Write-Host "API Key starts with:" $apiKey.Substring(0,10)
