b940fc1fd3e0f7da70c294de9774d81eaf1c9e1e
Paul Bullock <paul.bullock@capacreative.co.uk>
2022-03-23 10:39:30 +0000

    Initial connection established to on-premises

--- src/sdk/PnP.Core.Transformation.SharePoint.Test/TransformationTests.cs
+++ src/sdk/PnP.Core.Transformation.SharePoint.Test/TransformationTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.SharePoint.Client;
@@ -6,6 +7,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PnP.Core.Services;
 using PnP.Core.Transformation.Services.Core;
 using PnP.Core.Transformation.Test.Utilities;
+using PnP.Core.Auth;
+using PnP.Core.Transformation.SharePoint.Test.Utilities;
 
 namespace PnP.Core.Transformation.SharePoint.Test
 {
@@ -78,6 +81,81 @@ namespace PnP.Core.Transformation.SharePoint.Test
             Assert.IsNotNull(result);
             var expectedUri = new Uri($"{targetContext.Web.Url}/SitePages/Migrated_{sourceUri.Segments[sourceUri.Segments.Length - 1]}");
             Assert.AreEqual(expectedUri.AbsoluteUri, result.AbsoluteUri, ignoreCase: true);
+        }
+
+        [TestMethod]
+        public async Task SharePointTransformOnPremAsync()
+        {
+            var config = TestCommon.GetConfigurationSettings();
+
+            var services = new ServiceCollection();
+            services.AddTargetTestPnPCore();
+
+            // You can use the default settings
+            // services.AddPnPSharePointTransformation();
+
+            // Or you can provide a set of custom settings
+            services.AddPnPSharePointTransformation(
+                pnpOptions => // Global settings
+                {
+                    pnpOptions.DisableTelemetry = false;
+                    pnpOptions.PersistenceProviderConnectionString = config["PersistenceProviderConnectionString"];
+                },
+                pageOptions => // Target modern page creation settings
+                {
+                    pageOptions.CopyPageMetadata = true;
+                    pageOptions.KeepPageCreationModificationInformation = true;
+                    pageOptions.PostAsNews = false;
+                    pageOptions.PublishPage = false;
+                    pageOptions.DisablePageComments = false;
+                    pageOptions.KeepPageSpecificPermissions = true;
+                    pageOptions.Overwrite = true;
+                    pageOptions.ReplaceHomePageWithDefaultHomePage = true;
+                    pageOptions.SetAuthorInPageHeader = true;
+                    pageOptions.TargetPageFolder = "";
+                    pageOptions.TargetPageName = "";
+                    pageOptions.TargetPagePrefix = "Migrated_";
+                    pageOptions.TargetPageTakesSourcePageName = true;
+                },
+                spOptions => // SharePoint classic source settings
+                {
+                    // spOptions.WebPartMappingFile = config["WebPartMappingFile"];
+                    // spOptions.PageLayoutMappingFile = config["PageLayoutMappingFile"];
+                    spOptions.RemoveEmptySectionsAndColumns = true;
+                    spOptions.ShouldMapUsers = true;
+                    spOptions.HandleWikiImagesAndVideos = true;
+                    spOptions.AddTableListImageAsImageWebPart = true;
+                    spOptions.IncludeTitleBarWebPart = true;
+                    spOptions.MappingProperties = null;
+                    spOptions.SkipHiddenWebParts = true;
+                    spOptions.SkipUrlRewrite = true;
+                    spOptions.UrlMappings = null;
+                    spOptions.UserMappings = null;
+                }
+            );
+
+            var provider = services.BuildServiceProvider();
+
+            var pnpContextFactory = provider.GetRequiredService<IPnPContextFactory>();
+            var pageTransformator = provider.GetRequiredService<IPageTransformator>();
+
+            var targetContext = await pnpContextFactory.CreateAsync(TestCommon.TargetTestSite);
+            var sourceUri = new Uri(config["SourceUri"]);
+
+            var onPremCreds = TestCommon.ReadWindowsCredentialManagerEntry("OnPrem");
+            var onPremAuth = new OnPremisesAuth();
+
+            using (var sourceContext = onPremAuth.GetOnPremisesContext(config["SourceTestSite"], onPremCreds))
+            {
+
+                var result = await pageTransformator.TransformSharePointAsync(sourceContext, targetContext, sourceUri);
+                Console.WriteLine(result.AbsoluteUri);
+
+                Assert.IsNotNull(result);
+                var expectedUri = new Uri($"{targetContext.Web.Url}/SitePages/Migrated_{sourceUri.Segments[sourceUri.Segments.Length - 1]}");
+                Assert.AreEqual(expectedUri.AbsoluteUri, result.AbsoluteUri, ignoreCase: true);
+
+            }
         }
 
         [TestMethod]
