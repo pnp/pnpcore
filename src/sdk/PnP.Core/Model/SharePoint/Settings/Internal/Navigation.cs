677a376666efe31d72a45690615bd718c9f92490
Mathijs Verbeeck <Mathijs.Verbeeck@xylos.com>
2022-03-31 00:16:52 +0200

    Check in of the  navigation changes. Added both TopNavigation extension on the Web object and Quicklaunch navigation on the web object.

---
+++ src/sdk/PnP.Core/Model/SharePoint/Settings/Internal/Navigation.cs
@@ -0,0 +1,32 @@
+﻿using PnP.Core.Services;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PnP.Core.Model.SharePoint
+{
+
+    [SharePointType(NavigationConstants.NavigationMetadataType, Target = typeof(Web), Get = baseUri, LinqGet = baseUri)]
+    internal sealed class Navigation : BaseDataModel<INavigation>, INavigation
+    {
+        private const string baseUri = NavigationConstants.NavigationUri;
+
+        #region Properties        
+
+        public Guid Id { get => GetValue<Guid>(); set => Guid.NewGuid(); }
+
+        [SharePointProperty("UseShared")]
+        public bool UseShared { get => GetValue<bool>(); set => SetValue(value); }
+
+        public INavigationNodeCollection QuickLaunch { get => GetModelCollectionValue<INavigationNodeCollection>(NavigationType.QuickLaunch.ToString()); }
+        
+        public INavigationNodeCollection TopNavigationBar { get => GetModelCollectionValue<INavigationNodeCollection>(NavigationType.TopNavigationBar.ToString()); }
+
+        [KeyProperty(nameof(Id))]
+        public override object Key { get => Id; set => Id = Guid.Parse(value.ToString()); }
+
+        [SharePointProperty("*")]
+        public object All { get => null; }
+        #endregion
+    }
+}
---
+++ src/sdk/PnP.Core/Model/SharePoint/Settings/Public/INavigationNodeCollection.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace PnP.Core.Model.SharePoint
+{
+    /// <summary>
+    /// Represents the Navigation
+    /// </summary>
+    [ConcreteType(typeof(NavigationNodeCollection))]
+    public interface INavigationNodeCollection : IQueryable<INavigationNode>, IDataModelCollection<INavigationNode>, ISupportModules<INavigationNodeCollection>
+    {
+        /// <summary>
+        /// This variable will define whether we will be using the top navigation or the quick launch for our API calls
+        /// </summary>
+        public NavigationType NavigationType { get; }
+        // Add extension methods here
+        #region Get Methods
+
+        /// <summary>
+        /// Method to select a specific Navigation Node
+        /// </summary>
+        /// <param name="id">The Id to search for</param>
+        /// <param name="selectors">The expressions declaring the fields to select</param>
+        /// <returns>The navigation node, if any</returns>
+        public INavigationNode GetById(int id, params Expression<Func<INavigationNode, object>>[] selectors);
+
+        /// <summary>
+        /// Method to select a specific Navigation Node
+        /// </summary>
+        /// <param name="id">The Id to search for</param>
+        /// <param name="selectors">The expressions declaring the fields to select</param>
+        /// <returns>The navigation node, if any</returns>
+        public Task<INavigationNode> GetByIdAsync(int id, params Expression<Func<INavigationNode, object>>[] selectors);
+
+        #endregion
+
+        #region Add Methods
+        /// <summary>
+        /// Method to add a Navigation Node
+        /// </summary>
+        /// <param name="navigationNodeOptions">The options for the navigation node</param>
+        /// <returns>The navigation node</returns>
+        public INavigationNode Add(NavigationNodeOptions navigationNodeOptions);
+        
+        /// <summary>
+        /// Method to add a Navigation Node
+        /// </summary>
+        /// <param name="navigationNodeOptions">The options for the navigation node</param>
+        /// <returns>The navigation node</returns>
+        public Task<INavigationNode> AddAsync(NavigationNodeOptions navigationNodeOptions);
+        #endregion
+
+        #region Delete Methods
+
+        /// <summary>
+        /// Method to delete all navigation nodes from a specific navigation type
+        /// </summary>
+        /// <returns></returns>
+        public void DeleteAllNodes();
+
+        /// <summary>
+        /// Method to delete all navigation nodes from a specific navigation type
+        /// </summary>
+        /// <returns></returns>
+        public Task DeleteAllNodesAsync();
+
+        /// <summary>
+        /// Method to delete all navigation nodes from a specific navigation type in a batch
+        /// </summary>
+        /// <returns></returns>
+        public void DeleteAllNodesBatch();
+
+        /// <summary>
+        /// Method to delete all navigation nodes from a specific navigation type in a batch
+        /// </summary>
+        /// <returns></returns>
+        public Task DeleteAllNodesBatchAsync();
+        #endregion
+
+        #region Extension Methods
+
+        /// <summary>
+        /// Function to move a node after another navigation node
+        /// </summary>
+        /// <param name="nodeToMove"></param>
+        /// <param name="nodeToMoveAfter"></param>
+        /// <returns></returns>
+        public void MoveNodeAfter(INavigationNode nodeToMove, INavigationNode nodeToMoveAfter);
+
+        /// <summary>
+        /// Function to move a node after another navigation node
+        /// </summary>
+        /// <param name="nodeToMove"></param>
+        /// <param name="nodeToMoveAfter"></param>
+        /// <returns></returns>
+        public Task MoveNodeAfterAsync(INavigationNode nodeToMove, INavigationNode nodeToMoveAfter);
+        #endregion
+    }
+}
