341723a0d03fcaada75f52649d76e856f67f538e
Mathijs Verbeeck <Mathijs.Verbeeck@xylos.com>
2022-05-01 00:32:27 +0200

    Forgot to add comments for IIdentitySet

--- src/sdk/PnP.Core/Model/Security/Public/PermissionModels/IIdentitySet.cs
+++ src/sdk/PnP.Core/Model/Security/Public/PermissionModels/IIdentitySet.cs
@@ -1,21 +1,23 @@
 ﻿namespace PnP.Core.Model.Security
 {
     /// <summary>
-    /// 
+    /// The identitySet resource is a keyed collection of identity resources.
     /// </summary>
     [ConcreteType(typeof(IdentitySet))]
     public interface IIdentitySet
     {
         /// <summary>
-        /// 
+        /// The application associated with this action.
         /// </summary>
         public IIdentity Application { get; set; }
+
         /// <summary>
-        /// 
+        /// The device associated with this action.
         /// </summary>
         public IIdentity Device { get; set; }
+
         /// <summary>
-        /// 
+        /// The user associated with this action.
         /// </summary>
         public IIdentity User { get; set; }
     }
