c077f6df577b790559710ecbd41259dc5d647bb5
Bert Jansen <bjansen@microsoft.com>
2022-05-04 13:16:11 +0200

    Added support for translating sharepoint guids to drive item id and vice versa

---
+++ src/sdk/PnP.Core.Test/Misc/DriveHelperTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PnP.Core.Services;
+using PnP.Core.Utilities;
+using System;
+
+namespace PnP.Core.Test.Misc
+{
+    [TestClass()]
+    public class DriveHelperTests
+    {
+        [TestMethod()]
+        public void EncodeIdTest()
+        {
+            // File
+            Assert.AreEqual("01A7SUVGCLOOP3433QVRE2Z34DBGNLTPUC", DriveHelper.EncodeId(new Guid("b56adf79-ff6a-4964-a63a-ff1fa23be9f8"), 
+                                                                                       new Guid("8c8e101c-1b0d-4253-85e7-c30039bf46e2"), 
+                                                                                       new Guid("be9f734b-706f-49ac-acef-83099ab9be82")));
+            // Folder
+            Assert.AreEqual("01A7SUVGC7VA7ME76V55C3GRGEXYA7QF3X", DriveHelper.EncodeId(new Guid("b56adf79-ff6a-4964-a63a-ff1fa23be9f8"), 
+                                                                                       new Guid("8c8e101c-1b0d-4253-85e7-c30039bf46e2"), 
+                                                                                       new Guid("c23ea85f-d57f-45ef-b344-c4be01f81777")));
+        }
+
+        [TestMethod()]
+        public void DecodeIdTest()
+        {
+            // File
+            Assert.AreEqual(new Guid("be9f734b-706f-49ac-acef-83099ab9be82"), DriveHelper.DecodeId("01A7SUVGCLOOP3433QVRE2Z34DBGNLTPUC"));
+            
+            // Folder
+            Assert.AreEqual(new Guid("c23ea85f-d57f-45ef-b344-c4be01f81777"), DriveHelper.DecodeId("01A7SUVGC7VA7ME76V55C3GRGEXYA7QF3X"));
+        }
+
+    }
+}
---
+++ src/sdk/PnP.Core/Utilities/DriveHelper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace PnP.Core.Utilities
+{
+    internal static class DriveHelper
+    {
+        private const int ItemIdLength = 34;
+        private const int ItemIdPrefixLength = 2;
+        private const string ItemIdPrefix_V1 = "01"; 
+        private const int SizeOfGuid = 16;
+
+        internal static string EncodeId(Guid siteId, Guid webId, Guid docId)
+        {
+            byte[] hashBytes = ComputeItemIdHash(siteId, webId);
+
+            byte[] encodingBytes = new byte[SizeOfGuid + 4];
+
+            hashBytes.CopyTo(encodingBytes, 0); // 4 bytes
+            docId.ToByteArray().CopyTo(encodingBytes, 4);
+
+            return ItemIdPrefix_V1 + Base32Encoding.ConvertToBase32(encodingBytes);
+        }
+
+        internal static Guid DecodeId(string driveItemId)
+        {
+            // base32 maps to upper case.
+            string idPayload = driveItemId.Substring(ItemIdPrefixLength, ItemIdLength - ItemIdPrefixLength);
+            byte[] byteIds = Base32Encoding.ConvertFromBase32(idPayload.ToUpperInvariant()).ToArray();
+
+            byte[] hashBytes = new byte[4];
+            Array.Copy(byteIds, 0, hashBytes, 0, 4);
+            byte[] guidBytes = new byte[SizeOfGuid];
+            Array.Copy(byteIds, 4, guidBytes, 0, SizeOfGuid);
+
+            return new Guid(guidBytes);
+        }
+
+        private static byte[] ComputeItemIdHash(Guid siteId, Guid webId)
+        {
+            byte[] hashBytes = new byte[SizeOfGuid * 2];
+
+            siteId.ToByteArray().CopyTo(hashBytes, 0);
+            webId.ToByteArray().CopyTo(hashBytes, SizeOfGuid);
+            uint hashKey = ComputeHash(hashBytes);
+            return BitConverter.GetBytes(hashKey);
+        }
+
+        private static uint ComputeHash(byte[] bytes)
+        {
+            uint value = 0;
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] data = sha256Hash.ComputeHash(bytes);
+                for (int i = 0; i < sizeof(int); i++)
+                {
+                    value <<= 8;
+                    value |= data[i];
+                }
+            }
+
+            return value;
+        }
+
+    }
+}
