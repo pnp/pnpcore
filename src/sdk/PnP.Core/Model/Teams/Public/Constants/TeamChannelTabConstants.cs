099a6daccf5ddebdd2482caddfc5b4a9fbc84389
Daniel Pastoor <daniel.pastoor@puurict.nl>
2022-05-19 11:53:00 +0200

    Changed team channel tab constrants from interal to public so it is accesable by developers

---
+++ src/sdk/PnP.Core/Model/Teams/Public/Constants/TeamChannelTabConstants.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PnP.Core.Model.Teams
+{
+    public static class TeamChannelTabConstants
+    {
+        #region Constants
+
+        public const string TeamsAppId = "teamsAppId";
+        public const string WebAppId = "com.microsoft.teamspace.tab.web";
+        public const string DocumentLibraryAppId = "com.microsoft.teamspace.tab.files.sharepoint";
+        public const string WordAppId = "com.microsoft.teamspace.tab.file.staticviewer.word";
+        public const string ExcelAppId = "com.microsoft.teamspace.tab.file.staticviewer.excel";
+        public const string PptAppId = "com.microsoft.teamspace.tab.file.staticviewer.powerpoint";
+        public const string PdfAppId = "com.microsoft.teamspace.tab.file.staticviewer.pdf";
+
+        // no configuration tabs
+        public const string WikiAppId = "com.microsoft.teamspace.tab.wiki";
+        public const string PlannerAppId = "com.microsoft.teamspace.tab.planner";
+        public const string StreamsAppId = "com.microsoftstream.embed.skypeteamstab";
+        public const string FormsAppId = "81fef3a6-72aa-4648-a763-de824aeafb7d";
+        public const string OneNoteAppId = "0d820ecd-def2-4297-adad-78056cde7c78";
+        public const string PowerBiAppId = "com.microsoft.teamspace.tab.powerbi";
+        public const string SharePointPageOrListAppId = "2a527703-1f6f-4559-a332-d8a7d288cd88";
+        #endregion
+    }
+}
