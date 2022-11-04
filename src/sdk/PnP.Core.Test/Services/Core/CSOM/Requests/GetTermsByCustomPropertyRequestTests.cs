c683aca3edaf0763f1ed996e6b19257aab168300
Martin Loitzl <martin@loitzl.com>
2022-11-04 16:10:57 +0100

    cleanup

--- src/sdk/PnP.Core.Test/Services/Core/CSOM/Requests/GetTermsByCustomPropertyRequestTests.cs
+++ src/sdk/PnP.Core.Test/Services/Core/CSOM/Requests/GetTermsByCustomPropertyRequestTests.cs
@@ -23,37 +23,7 @@ namespace PnP.Core.Test.Services.Core.CSOM.Requests
         public static void TestFixtureSetup(TestContext context)
         {
             // Configure mocking default for all tests in this class, unless override by a specific test
-            TestCommon.Instance.Mocking = false;
-        }
-
-        [TestMethod]
-        public async Task GetTermsByCustomProperty()
-        {
-
-            using (var context = await TestCommon.Instance.GetContextAsync(TestCommon.TestSite))
-            {
-                var termStore = await context.TermStore.GetAsync();
-                var termGroup = await termStore.Groups.GetByNameAsync("People");
-                var termSet = await termGroup.Sets.GetByIdAsync("8ed8c9ea-7052-4c1d-a4d7-b9c10bffea6f");
-            }
-        }
-
-        [TestMethod]
-        public void GetTermsByCustomPropertyRequest_Test_GetCorrectRequest()
-        {
-            GetTermsByCustomPropertyRequest request = new GetTermsByCustomPropertyRequest("key1", "value1", false);
-
-            var requests = request.GetRequest(new IteratorIdProvider());
-            var actionRequests = requests.Select(r => r.Action).ToList();
-            var identities = requests.Select(r => r.ObjectPath).Where(id => id != null).ToList();
-
-            string requestBody = "";
-
-            CSOMApiCallBuilder csomAPICallBuilder = new CSOMApiCallBuilder();
-            csomAPICallBuilder.AddRequest(request);
-
-            requestBody = csomAPICallBuilder.SerializeCSOMRequests();
-
+            //TestCommon.Instance.Mocking = false;
         }
 
         [TestMethod]
