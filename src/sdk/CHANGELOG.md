ef5fda5b6d697bc0e53124e42703e165aa662f30
Bert Jansen <bjansen@microsoft.com>
2022-12-20 15:04:58 +0100

    Improve code consistency for the eventhub model

--- src/sdk/CHANGELOG.md
+++ src/sdk/CHANGELOG.md
@@ -24,6 +24,7 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/).
 - Fix modern team site creation when no valid description was passed in #1037 [LeHailender - Jens Haile]
 - Fix for adding site collection term group #1041 [jansenbe - Bert Jansen]
 - The JSON controldata sectionindex value of page could not be converted to System.Int32 : Handle Pnp Provisioning template extraction failure from Project Management site template #1058 [stokuri - Suman Tokori]
+- Improve code consistency for the eventhub model [jansenbe - Bert Jansen]
 
 ## [1.8.0]
 
---
+++ src/sdk/PnP.Core/Services/Core/IRetryEvent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PnP.Core.Services
+{
+    /// <summary>
+    /// Event containing information about the happened retry
+    /// </summary>
+    public interface IRetryEvent
+    {
+        /// <summary>
+        /// Request that was retried
+        /// </summary>
+        Uri Request { get; }
+
+        /// <summary>
+        /// Http status code for the retried request
+        /// </summary>
+        int HttpStatusCode { get; }
+
+        /// <summary>
+        /// Wait before the next try in seconds
+        /// </summary>
+        int WaitTime { get; }
+
+        /// <summary>
+        /// SocketException that triggered the retry
+        /// </summary>
+        Exception Exception { get; }
+
+        /// <summary>
+        /// Event property bag
+        /// </summary>
+        IDictionary<string, object> Properties { get; }
+    }
+}
