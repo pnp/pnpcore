0804694fdfde15b02c16bb61b1e42e1796ce58ea
Martin Loitzl <martin@loitzl.com>
2023-06-12 21:18:15 +0200

    filter out alreadyGranted PermissionGrants

--- src/sdk/PnP.Core.Admin/Model/SharePoint/Core/Internal/ServicePrincipal.cs
+++ src/sdk/PnP.Core.Admin/Model/SharePoint/Core/Internal/ServicePrincipal.cs
@@ -2,7 +2,9 @@ using PnP.Core.Admin.Services.Core.CSOM.Requests.ServicePrincipal;
 using PnP.Core.Model.SharePoint;
 using PnP.Core.Services;
 using PnP.Core.Services.Core.CSOM.Requests;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -86,117 +88,145 @@ namespace PnP.Core.Admin.Model.SharePoint
                 .RawRequestAsync(getPermissionRequestsCall, HttpMethod.Post)
                 .ConfigureAwait(false);
 
-            var getPrincipalIdCall =
-                new ApiCall(
-                    "servicePrincipals/?$filter=displayName eq 'SharePoint Online Client Extensibility Web Application Principal'",
-                    ApiType.Graph);
+            string appPrincipalId = await GetSharePointAppPrincipalId().ConfigureAwait(false);
             
-            var result = await (tenantAdminContext.Web as Web)
-                .RawRequestAsync(getPrincipalIdCall, HttpMethod.Get)
-                .ConfigureAwait(false);
+            List<IPermissionRequest> permissionRequests =  (List<IPermissionRequest>) csomResult.ApiCall.CSOMRequests[0].Result;
 
-
-            #region JsonResponse
-            
-             /*
-             {
-              "@odata.context": "https://graph.microsoft.com/v1.0/$metadata#servicePrincipals",
-              "value": [
-                {
-                  "id": "9b994571-2cf2-4fa4-acd0-9fe1f271410f",
-                  "accountEnabled": false,
-                  "alternativeNames": [],
-                  "appDisplayName": "SharePoint Online Client Extensibility Web Application Principal",
-                  "appId": "40ed0677-9e6f-435c-b198-7634beba3874",
-                  "appOwnerOrganizationId": "60df2466-a102-404a-8d9d-95c950626730",
-                  "appRoleAssignmentRequired": false,
-                  "createdDateTime": "2021-05-12T10:22:37Z",
-                  "displayName": "SharePoint Online Client Extensibility Web Application Principal",
-                  "notificationEmailAddresses": [],
-                  "replyUrls": [
-                    "https://fluidpreview.office.net/spfxsinglesignon",
-                    "https://dev.fluidpreview.office.net/spfxsinglesignon",
-                    "https://loitzl2-admin.sharepoint.com/_forms/spfxsinglesignon.aspx",
-                    "https://loitzl2.sharepoint.com/",
-                    "https://loitzl2.sharepoint.com/_forms/spfxsinglesignon.aspx",
-                    "https://loitzl2.sharepoint.com/_forms/spfxsinglesignon.aspx?redirect"
-                  ],
-                  "servicePrincipalNames": [
-                    "api://60df2466-a102-404a-8d9d-95c950626730/microsoft.spfx3rdparty.com",
-                    "40ed0677-9e6f-435c-b198-7634beba3874"
-                  ],
-                  "servicePrincipalType": "Application",
-                  "signInAudience": "AzureADMyOrg",
-                  "tags": [],
-                  "addIns": [],
-                  "appRoles": [],
-                  "info": {},
-                  "keyCredentials": [],
-                  "oauth2PermissionScopes": [
-                    {
-                      "adminConsentDescription": "Allow the application to access SharePoint Online Client Extensibility Web Application Principal on behalf of the signed-in user.",
-                      "adminConsentDisplayName": "Access SharePoint Online Client Extensibility Web Application Principal",
-                      "id": "80908136-1ead-4acd-acdb-c006ba6072bd",
-                      "isEnabled": true,
-                      "type": "User",
-                      "userConsentDescription": "Allow the application to access SharePoint Online Client Extensibility Web Application Principal on your behalf.",
-                      "userConsentDisplayName": "Access SharePoint Online Client Extensibility Web Application Principal",
-                      "value": "user_impersonation"
-                    }
-                  ],
-                  "passwordCredentials": [],
-                  "resourceSpecificApplicationPermissions": [],
-                  "verifiedPublisher": {}
-                }
-              ]
-            }
-            */
-             
-            #endregion
-
-            var root = JsonDocument.Parse(result.Json).RootElement.GetProperty("value");
-
-            if (root.ValueKind == JsonValueKind.Array)
+            if (string.IsNullOrWhiteSpace(appPrincipalId))
             {
-                foreach (var siteInformation in root.EnumerateArray())
-                {
-                    if (siteInformation.TryGetProperty("id", out JsonElement id))
+                return permissionRequests;
+            }
+            
+            List<string> alreadyGranted =  await GetSharePointAppPrincipalPermissionGrants(appPrincipalId).ConfigureAwait(false);
+            
+            return permissionRequests
+                .Where(permissionRequest => !alreadyGranted.Contains(permissionRequest.Scope))
+                .ToList();
+
+            async Task<string> GetSharePointAppPrincipalId()
+            {
+                ApiCall getPrincipalIdCall =
+                    new ApiCall(
+                        "servicePrincipals/?$filter=displayName eq 'SharePoint Online Client Extensibility Web Application Principal'",
+                        ApiType.Graph);
+
+                ApiCallResponse result = await (tenantAdminContext.Web as Web)
+                    .RawRequestAsync(getPrincipalIdCall, HttpMethod.Get)
+                    .ConfigureAwait(false);
+
+                #region JsonResponse
+
+                /*
+                 {
+                  "@odata.context": "https://graph.microsoft.com/v1.0/$metadata#servicePrincipals",
+                  "value": [
                     {
-                        var getOAuth2PermissionGrantsCall =
-                            new ApiCall(
-                                $"oAuth2Permissiongrants/?$filter=clientId eq '{id}' and consentType eq 'AllPrincipals'",
-                                ApiType.Graph);
-
-                        result = await (tenantAdminContext.Web as Web)
-                            .RawRequestAsync(getOAuth2PermissionGrantsCall, HttpMethod.Get)
-                            .ConfigureAwait(false);
-
-                        #region JsonResponse
-                        /*
+                      "id": "9b994571-2cf2-4fa4-acd0-9fe1f271410f",
+                      "accountEnabled": false,
+                      "alternativeNames": [],
+                      "appDisplayName": "SharePoint Online Client Extensibility Web Application Principal",
+                      "appId": "40ed0677-9e6f-435c-b198-7634beba3874",
+                      "appOwnerOrganizationId": "60df2466-a102-404a-8d9d-95c950626730",
+                      "appRoleAssignmentRequired": false,
+                      "createdDateTime": "2021-05-12T10:22:37Z",
+                      "displayName": "SharePoint Online Client Extensibility Web Application Principal",
+                      "notificationEmailAddresses": [],
+                      "replyUrls": [
+                        "https://fluidpreview.office.net/spfxsinglesignon",
+                        "https://dev.fluidpreview.office.net/spfxsinglesignon",
+                        "https://loitzl2-admin.sharepoint.com/_forms/spfxsinglesignon.aspx",
+                        "https://loitzl2.sharepoint.com/",
+                        "https://loitzl2.sharepoint.com/_forms/spfxsinglesignon.aspx",
+                        "https://loitzl2.sharepoint.com/_forms/spfxsinglesignon.aspx?redirect"
+                      ],
+                      "servicePrincipalNames": [
+                        "api://60df2466-a102-404a-8d9d-95c950626730/microsoft.spfx3rdparty.com",
+                        "40ed0677-9e6f-435c-b198-7634beba3874"
+                      ],
+                      "servicePrincipalType": "Application",
+                      "signInAudience": "AzureADMyOrg",
+                      "tags": [],
+                      "addIns": [],
+                      "appRoles": [],
+                      "info": {},
+                      "keyCredentials": [],
+                      "oauth2PermissionScopes": [
                         {
-                          "@odata.context": "https://graph.microsoft.com/v1.0/$metadata#oauth2PermissionGrants",
-                          "value": [
-                            {
-                              "clientId": "9b994571-2cf2-4fa4-acd0-9fe1f271410f",
-                              "consentType": "AllPrincipals",
-                              "id": "cUWZm_IspE-s0J_h8nFBDzH0zhmfRphIjnLf0TBUs_U",
-                              "resourceId": "19cef431-469f-4898-8e72-dfd13054b3f5",
-                              "scope": "User.ReadBasic.All"
-                            }
-                          ]
-                        } 
-                        */
-                        #endregion
-
-                        root = JsonDocument.Parse(result.Json).RootElement.GetProperty("value");
-
-                        //todo: filter (List<IPermissionRequest>)csomResult.ApiCall.CSOMRequests[0].Result
-                        //        by JsonDocument.Parse(result.Json).RootElement.GetProperty("value")
+                          "adminConsentDescription": "Allow the application to access SharePoint Online Client Extensibility Web Application Principal on behalf of the signed-in user.",
+                          "adminConsentDisplayName": "Access SharePoint Online Client Extensibility Web Application Principal",
+                          "id": "80908136-1ead-4acd-acdb-c006ba6072bd",
+                          "isEnabled": true,
+                          "type": "User",
+                          "userConsentDescription": "Allow the application to access SharePoint Online Client Extensibility Web Application Principal on your behalf.",
+                          "userConsentDisplayName": "Access SharePoint Online Client Extensibility Web Application Principal",
+                          "value": "user_impersonation"
+                        }
+                      ],
+                      "passwordCredentials": [],
+                      "resourceSpecificApplicationPermissions": [],
+                      "verifiedPublisher": {}
                     }
+                  ]
                 }
+                */
+
+                #endregion
+
+                JsonElement root = JsonDocument.Parse(result.Json).RootElement.GetProperty("value");
+
+                if (root.ValueKind != JsonValueKind.Array)
+                {
+                    return String.Empty;
+                }
+
+                return root
+                    .EnumerateArray()
+                    .Select(principal => principal.GetProperty("id").GetString())
+                    .FirstOrDefault();
             }
 
-            return (List<IPermissionRequest>)csomResult.ApiCall.CSOMRequests[0].Result;
+            async Task<List<string>> GetSharePointAppPrincipalPermissionGrants(string id)
+            {
+                var getOAuth2PermissionGrantsCall =
+                    new ApiCall(
+                        $"oAuth2PermissionGrants/?$filter=clientId eq '{id}' and consentType eq 'AllPrincipals'",
+                        ApiType.Graph);
+
+                var result = await (tenantAdminContext.Web as Web)
+                    .RawRequestAsync(getOAuth2PermissionGrantsCall, HttpMethod.Get)
+                    .ConfigureAwait(false);
+
+                #region JsonResponse
+
+                /*
+                {
+                  "@odata.context": "https://graph.microsoft.com/v1.0/$metadata#oauth2PermissionGrants",
+                  "value": [
+                    {
+                      "clientId": "9b994571-2cf2-4fa4-acd0-9fe1f271410f",
+                      "consentType": "AllPrincipals",
+                      "id": "cUWZm_IspE-s0J_h8nFBDzH0zhmfRphIjnLf0TBUs_U",
+                      "resourceId": "19cef431-469f-4898-8e72-dfd13054b3f5",
+                      "scope": "User.ReadBasic.All"
+                    }
+                  ]
+                } 
+                */
+
+                #endregion
+
+                var root = JsonDocument.Parse(result.Json).RootElement.GetProperty("value");
+
+                if (root.ValueKind != JsonValueKind.Array)
+                {
+                    return new List<string>();
+                }
+                
+                return root
+                    .EnumerateArray()
+                    .Select(element => element.GetProperty("scope").GetString())
+                    .ToList();
+            }
         }
 
         public async Task<IServicePrincipalProperties> EnableAsync(VanityUrlOptions vanityUrlOptions = null)
