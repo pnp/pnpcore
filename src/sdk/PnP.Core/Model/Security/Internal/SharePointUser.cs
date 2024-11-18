pnp/pnpcore

commit 29a64bef0cfedbb3a0f7b64386b64d54793d7d71

Fix loading additional properties of an `ISharePointUser` #1553

src/sdk/PnP.Core/Model/Security/Internal/SharePointUser.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace PnP.Core.Model.Security
 {
     [SharePointType("SP.User", Target = typeof(SharePointGroup), Uri = "_api/Web/sitegroups/getbyid({Parent.Id})/users/getbyid({Id})", Get = "_api/Web/sitegroups/getbyid({Parent.Id})/users", LinqGet = "_api/Web/sitegroups/getbyid({Parent.Id})/users")]
-    [SharePointType("SP.User", Target = typeof(Web), Uri = "_api/Web/GetUserById({Id})", Get = "_api/Web/SiteUsers", LinqGet = "_api/Web/SiteUsers")]
+    [SharePointType("SP.User", Target = typeof(Web), Uri = "_api/Web/GetUserById({Id})", LinqGet = "_api/Web/SiteUsers")]
     internal sealed class SharePointUser : BaseDataModel<ISharePointUser>, ISharePointUser
     {
         #region Construction
@@ -28,7 +28,7 @@ namespace PnP.Core.Model.Security
                         LoginName
                     };
                     var jsonBody = JsonSerializer.Serialize(body);
-                    return new ApiCall(entity.SharePointGet, ApiType.SPORest, jsonBody);
+                    return new ApiCall("_api/Web/SiteUsers", ApiType.SPORest, jsonBody);
 
                 }).ConfigureAwait(false);
             };

commit 5ee8a20aae52da52d7da9aa5513da960f68166f8

Fixed regression introduced by https://github.com/pnp/pnpcore/commit/29a64bef0cfedbb3a0f7b64386b64d54793d7d71 and was partly fixed by #1577

src/sdk/PnP.Core/Model/Security/Internal/SharePointUser.cs
@@ -28,7 +28,16 @@ namespace PnP.Core.Model.Security
                         LoginName
                     };
                     var jsonBody = JsonSerializer.Serialize(body);
-                    return new ApiCall(entity.SharePointGet, ApiType.SPORest, jsonBody);
+                    
+                    if (entity.Target == typeof(Web))
+                    {
+                        return new ApiCall("_api/Web/SiteUsers", ApiType.SPORest, jsonBody);
+                    }
+                    else
+                    {
+                        return new ApiCall(entity.SharePointGet, ApiType.SPORest, jsonBody);
+                    }
+                    
 
                 }).ConfigureAwait(false);
             };