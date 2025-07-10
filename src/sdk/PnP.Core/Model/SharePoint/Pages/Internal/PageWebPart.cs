42fdef035529cb50f2f7bde9fdbc546985d484ee
Pedro Monte <pedro@bindtuning.com>
2025-07-10 11:30:45 +0100

    Added validation if the jsonSnippet is already escaped or not

--- src/sdk/PnP.Core/Model/SharePoint/Pages/Internal/PageWebPart.cs
+++ src/sdk/PnP.Core/Model/SharePoint/Pages/Internal/PageWebPart.cs
@@ -672,18 +672,31 @@ namespace PnP.Core.Model.SharePoint
         private static string EscapeJsonValues(string decodedWebPart)
         {
             // regular expression to find a JSON string value (property with html content example: data-config-json=\"{ "k1":"v1", "k2":"{v2}", "k3": ["k4":"v4","k5":"v5"] }\" )
-            System.Text.RegularExpressions.Regex regex = new(@"\\""{("".+?)}\\""", System.Text.RegularExpressions.RegexOptions.Singleline);
+            System.Text.RegularExpressions.Regex regex = new(@"\\""({"".+?})\\""", System.Text.RegularExpressions.RegexOptions.Singleline);
             // get all matches
             System.Text.RegularExpressions.MatchCollection matches = regex.Matches(decodedWebPart);
-            // iterate over all matches
             if (matches.Count > 0)
             {
+                string jsonSnippet = string.Empty;
+                // iterate over all matches
                 foreach (System.Text.RegularExpressions.Match match in matches)
                 {
+                    jsonSnippet = match.Groups[1].Value;
+                    // Try to parse the JSON to see if it's valid
+                    try
+                    {
+                        _ = JsonDocument.Parse(jsonSnippet); // success = unescaped
+                    }
+                    catch
+                    {
+                        // Already escaped or malformed – don't re-escape
+                        continue;
+                    }
+
                     // replace all double quotes with escaped double quotes
-                    var escapedMatch = match.Groups[1].Value.Replace("\"", "\\\"");
+                    var escapedSnipped = jsonSnippet.Replace("\"", "\\\"");
                     // replace the original match with the escaped match
-                    decodedWebPart = decodedWebPart.Replace(match.Groups[1].Value, escapedMatch);
+                    decodedWebPart = decodedWebPart.Replace(jsonSnippet, escapedSnipped);
                 }
             }
 
