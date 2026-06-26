c35ebf8a540b2574de2852882d24a7a3d439c86a
Gautam Sheth <gautamdsheth@outlook.com>
2026-06-26 22:58:03 +0300

    Rename sovereign cloud environments in CloudManager and Microsoft365Environment (#1787)
    Renaming it align with 1P tooling and avoid confusion

--- src/sdk/PnP.Core/Services/Core/CloudManager.cs
+++ src/sdk/PnP.Core/Services/Core/CloudManager.cs
@@ -12,10 +12,10 @@ namespace PnP.Core.Services
             {
                 "com" => Microsoft365Environment.Production,
                 "us" => Microsoft365Environment.USGovernment,
-                "de" => Microsoft365Environment.GovDe,
+                "de" => Microsoft365Environment.DelosCloud,
                 "cn" => Microsoft365Environment.China,
-                "fr" => Microsoft365Environment.GovFr,
-                "sg" => Microsoft365Environment.GovSg,                
+                "fr" => Microsoft365Environment.BleuCloud,
+                "sg" => Microsoft365Environment.GovSGCloud,                
                 _ => Microsoft365Environment.Production,
             };
         }
@@ -36,9 +36,9 @@ namespace PnP.Core.Services
                 Microsoft365Environment.USGovernmentHigh => "graph.microsoft.us",
                 Microsoft365Environment.USGovernmentDoD => "dod-graph.microsoft.us",                
                 Microsoft365Environment.China => "microsoftgraph.chinacloudapi.cn",
-                Microsoft365Environment.GovFr => "graph.svc.sovcloud.fr",
-                Microsoft365Environment.GovDe => "graph.svc.sovcloud.de",
-                Microsoft365Environment.GovSg => "graph.svc.sovcloud.sg",
+                Microsoft365Environment.BleuCloud => "graph.svc.sovcloud.fr",
+                Microsoft365Environment.DelosCloud => "graph.svc.sovcloud.de",
+                Microsoft365Environment.GovSGCloud => "graph.svc.sovcloud.sg",
                 _ => "graph.microsoft.com"
             };
         }
@@ -59,9 +59,9 @@ namespace PnP.Core.Services
                 Microsoft365Environment.USGovernmentHigh => "login.microsoftonline.us",
                 Microsoft365Environment.USGovernmentDoD => "login.microsoftonline.us",                
                 Microsoft365Environment.China => "login.chinacloudapi.cn",
-                Microsoft365Environment.GovFr => "login.sovcloud-identity.fr",
-                Microsoft365Environment.GovDe => "login.sovcloud-identity.de",
-                Microsoft365Environment.GovSg => "login.sovcloud-identity.sg",
+                Microsoft365Environment.BleuCloud => "login.sovcloud-identity.fr",
+                Microsoft365Environment.DelosCloud => "login.sovcloud-identity.de",
+                Microsoft365Environment.GovSGCloud => "login.sovcloud-identity.sg",
                 _ => "login.microsoftonline.com"
             };
         }
